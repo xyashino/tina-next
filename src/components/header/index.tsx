22700fb75849d1ddd2fb4c44a00b9bdd0b36c4ff
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-11-21 21:56:53 +0100

    fix: improve styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,13 @@
 import { DesktopNavigation } from '@/components/header/desktop-navigation'
 import { MobileNavigation } from '@/components/header/mobile-navigation'
-import { CrossIcon } from '@/components/icons/cross-icon'
 import { getNavigation } from '@/services/navigation.service'
 
 export const Header = async () => {
   const navigation = await getNavigation()
   return (
     <nav className="mb-10 flex min-h-10 w-full items-center justify-between border-b pb-5 pt-4 sm:min-h-14 lg:justify-around px-4">
-      <a className="group flex items-center text-foreground" href="/">
-        <CrossIcon className="size-6" />
-        <span className="translate-y-[10%] text-lg lg:text-xl font-semibold group-hover:underline content">
-          Gruszow Wielki
-        </span>
+      <a className="text-xl font-semibold hover:underline" href="/">
+        Gruszow Wielki
       </a>
       <DesktopNavigation data={navigation} />
       <MobileNavigation data={navigation} />
