2c2fd4580359567c30202a30b13c694e69cb6a54
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-11-21 22:24:05 +0100

    dev (#2)
    * chore: partial mirgation from astro
    
    * feat: implement navigation
    
    * feat: implement intentions
    
    * feat: implement gallery
    
    * fix: improve mobile styles
    
    * feat: implement visual editing for contact page
    
    * feat: implement pages collection
    
    * feat: imporve styles
    
    * fix: add not-prose classname
    
    * feat: add image to page collection
    
    * feat: add annoucments collection
    
    * fix: improve types
    
    * fix: improve styles
    
    * chore: init tina cloud
    
    * fix: minor issues

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -619,8 +619,9 @@ var pageCollection = {
 };
 
 // tina/config.ts
+var branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || process.env.HEAD || "";
 var config_default = defineConfig({
-  branch: "main",
+  branch,
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
   token: process.env.TINA_TOKEN || "",
   build: {
